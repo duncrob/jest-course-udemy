a55295124e7bfdd031470daf998f61acb04dc395
duncrob <duncrob@uw.edu>
2023-07-05 02:04:01 -0700

    Created basic React app from scratch

--- users/src/UserForm.js
+++ users/src/UserForm.js
@@ -8,21 +8,21 @@ function UserForm({ onUserAdd }) {
     event.preventDefault();
 
     onUserAdd({ name, email });
-  };
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Name</label>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <input value={name} onChange={e => setName(e.target.value)} />
       </div>
       <div>
         <label>Email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input value={email} onChange={e => setEmail(e.target.value)} />
       </div>
       <button>Add User</button>
     </form>
-  );
+  )
 }
 
 export default UserForm;
--- users/src/UserList.js
+++ users/src/UserList.js
@@ -1,11 +1,9 @@
 function UserList({ users }) {
   const renderedUsers = users.map((user) => {
-    return (
-      <tr key={user.name}>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-      </tr>
-    );
+    return <tr key={user.name}>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+    </tr>
   });
 
   return (
